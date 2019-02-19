62d611f9e57d6d53f799304d7cd50cfe8812ed34
Krista Handel <k.handel66@gmail.com>
2019-02-18 17:28:25 -0700

    Setup pages skeleton

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default class Index extends React.Component {
+	render() {
+		return (
+			<Head>
+				<title>secretnodes.org</title>
+			</Head>	
+		)	
+	}
+}
