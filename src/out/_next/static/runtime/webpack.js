2442c5b9a364b42223b499769db0c599f768f9d7
Krista Handel <k.handel66@gmail.com>
2019-02-21 21:52:47 -0700

    build and export locally

--- src/out/_next/static/runtime/webpack.js
+++ src/out/_next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bd409eed2c9593d58f9";
+/******/ 	var hotCurrentHash = "722d5d898809ca1cf7cc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
