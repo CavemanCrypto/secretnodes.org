4ede524802b4730b0300c651e1450463021125a8
moonstash <iandixon@Ians-MacBook-Pro.local>
2019-02-23 00:19:21 -0600

    index
    index

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -35,9 +35,6 @@ const OverviewSection = props => {
       <TextContainer mediaQueries={props.mediaQueries}>
 				Leo a diam sollicitudin tempor id eu. Pellentesque dignissim enim sit amet ve     nenatis urna cursus eget. Nisi est sit amet facilisis magna etiam. Congue eu consequa     t ac felis donec. Nibh nisl condimentum id venenatis a condimentum vitae sapien pelle     ntesque. Quisque non tellus orci ac auctor augue mauris. Faucibus pulvinar elementum      integer enim neque volutpat ac tincidunt. Vel facilisis volutpat est velit egestas du     i id ornare. Ac auctor augue mauris augue neque gravida. Sit amet nulla facilisi morb     i tempus.
       </TextContainer>
-      <TextContainer mediaQueries={props.mediaQueries}>
-        2 Leo a diam sollicitudin tempor id eu. Pellentesque dignissim enim sit amet ve     nenatis urna cursus eget. Nisi est sit amet facilisis magna etiam. Congue eu consequa     t ac felis donec. Nibh nisl condimentum id venenatis a condimentum vitae sapien pelle     ntesque. Quisque non tellus orci ac auctor augue mauris. Faucibus pulvinar elementum      integer enim neque volutpat ac tincidunt. Vel facilisis volutpat est velit egestas du     i id ornare. Ac auctor augue mauris augue neque gravida. Sit amet nulla facilisi morb     i tempus.
-      </TextContainer>
     </Overview>
   )
 }
