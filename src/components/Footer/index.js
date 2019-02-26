f940c9cafa4e6bbb07f48e8166818b453b6f41b4
moonstash <iandixon@Ians-MacBook-Pro.local>
2019-02-25 21:44:00 -0600

    credit-update
    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -74,7 +74,7 @@ const PageFooter = props => {
           <span className='span-copyright'>secretnodes.org©</span>
           <span className='secret-nodes-motto'>empowering people to secure the privacy layer of the web since 2018</span>
         </div>
-				<span className='special-span'><a href='https://www.moonstash.io' rel='noopener noreferrer'>built by 🐉s at the moonstash think tank</a></span>
+				<span className='special-span'><a href='https://www.moonstash.io' rel='noopener noreferrer'>built by the moonstash think tank</a></span>
       </div>
     </Footer>
   )
