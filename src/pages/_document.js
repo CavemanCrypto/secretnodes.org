20abc4e23dfa96f82ac0ca963c68ef236b0d2fc9
moonstash <iandixon@Ians-MacBook-Pro.local>
2019-02-25 23:54:45 -0600

    fix
    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/lib   s/normalize/8.0.0/normalize.min.css' />
+          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css' />
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         </Head>
         <body className='custom-class'>
