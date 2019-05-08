06c28606b49740b76fdb080e7c2d210476f083d0
Ian <15162813+moonstash@users.noreply.github.com>
2019-05-08 02:32:40 -0700

    calculator_table test fix
    Testing

---
+++ src/components/Calculator/calculator_table.js
@@ -0,0 +1,131 @@
+import numeral from 'numeral'
+import styled from '@emotion/styled'
+
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
+
+const CalculatorOutputSection = styled('section')({
+
+  letterSpacing: '.3px',
+  height: 'auto',
+  width: '100%',
+  background: '#060b47',
+  color: '#fbfbfd',
+  minHeight: 150,
+  //minHeight: 300,
+  padding: '50px 0px',
+
+  table: {
+    width: '100%',
+    //borderSpacing: '.5rem',
+    background: '#065198',
+    //border: 'solid thin',
+    borderCollapse: 'collapse',
+  },
+
+})
+
+const data = [];
+
+function setData (state, period, currency) {
+
+  var formatter = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  })
+
+  data.push({
+    id: period,
+    profit: formatter.format(state[period].profit),
+    earned: numeral(state[period].earned).format('0,0'),
+    cost: formatter.format(state[period].cost),
+    roi: numeral(state[period].roi).format('(0.00 %)'),
+  });
+}
+
+function updateData (state, period, currency) {
+
+  const index = {
+    //id: 0,
+    daily: 0,
+    weekly: 1,
+    monthly: 2,
+    yearly: 3,
+  }
+
+  var formatter = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  })
+
+  data[index[period]].profit=formatter.format(state[period].profit)
+  data[index[period]].earned=numeral(state[period].earned).format('0,0')
+  data[index[period]].cost=formatter.format(state[period].cost)
+  data[index[period]].roi=numeral(state[period].roi).format('(0.00 %)')
+
+}
+
+const calculateDaily = (inputValues, constantValues, days = 1) => {
+  const daysInYear = 365.0
+  const dailyCost = inputValues.yearlyNodeCost / daysInYear
+  const dailyRewards = constantValues.yearlyRewards / daysInYear
+  const dailyEngEarned = dailyRewards * (inputValues.nodeStake / inputValues.totalGenisisNodeStake)
+  const dailyRevenue = dailyEngEarned * inputValues.ENGPrice
+  const dailyProfit = dailyRevenue - dailyCost
+  const dailyReturn = (dailyEngEarned / inputValues.nodeStake) - (dailyCost * inputValues.ENGPrice)
+
+  return {
+    revenue: dailyRevenue * days,
+    profit: dailyProfit * days,
+    earned: dailyEngEarned * days,
+    cost: dailyCost * days,
+    roi: dailyReturn * days,
+  }
+}
+//
+const CalculatorOutput = props => {
+  const { constantValues } = props
+  const { currency } = props
+
+  const calculateAll = () => ({
+    daily: calculateDaily(props.inputValues, constantValues),
+    weekly: calculateDaily(props.inputValues, constantValues, 7),
+    monthly: calculateDaily(props.inputValues, constantValues, 30.416666),
+    yearly: calculateDaily(props.inputValues, constantValues, 365)
+  })
+
+  const state = calculateAll()
+
+  {Object.keys(state).map(period => {
+    if(data.length < 4 ) { setData(state, period, currency) }
+      else {
+          updateData(state, period, currency)
+      }
+  })}
+
+  const options = {
+      noDataText: 'ERROR: No data in Enigma table!'
+    }
+
+  return (
+    <div>
+    <CalculatorOutputSection>
+          <BootstrapTable data= { data } options={ options }
+          tableStyle={ { border: 'solid thin' }  }
+          containerStyle={ { border: 'solid thin', width: '100%' } }
+          border={ true }>
+            <TableHeaderColumn width='150' dataField='id' isKey={ true } headerAlign={'center'} tdStyle={ { 'border': 'solid thin', 'padding': '5px'} }>Period</TableHeaderColumn>
+            <TableHeaderColumn width='150' dataField='profit' headerAlign={'center'} tdStyle={ { 'border': 'solid thin', 'padding': '2px'} }>Profit</TableHeaderColumn>
+            <TableHeaderColumn width='150' dataField='earned' headerAlign={'center'} tdStyle={ { 'border': 'solid thin', 'padding': '2px'} }>ENG Earned</TableHeaderColumn>
+            <TableHeaderColumn width='150' dataField='cost' headerAlign={'center'} tdStyle={ { 'border': 'solid thin', 'padding': '2px'} }>Node Cost</TableHeaderColumn>
+            <TableHeaderColumn width='150' dataField='roi' headerAlign={'center'} tdStyle={ { 'border': 'solid thin', 'padding': '2px'} }>ROI</TableHeaderColumn>
+          </BootstrapTable>
+    </CalculatorOutputSection>
+    </div>
+    )
+}
+
+export default CalculatorOutput
