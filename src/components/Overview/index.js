f9c3b20a6bf19f3eb6cce26b7c20d9bb52834d30
Ian <15162813+moonstash@users.noreply.github.com>
2019-05-08 19:56:48 -0700

    update overview index
    bold

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -32,7 +32,7 @@ const OverviewSection = props => {
 Enigma is building a privacy layer for the decentralized web. First researched at MIT, Enigma’s network of “secret nodes” can perform computations over encrypted data at scale without ever exposing the raw data itself. By finally enabling decentralized applications to use private and sensitive data, smart contracts built with Enigma become “secret contracts”, allowing developers to create thousands of new, valuable solutions.
       </TextContainer>
       <TextContainer mediaQueries={props.mediaQueries}>
-Role and Utility of ENG
+<b>Role and Utility of ENG</b>
 The ENG token is critical to the function of the Enigma network, playing two primary roles: as payment for computation and as a requirement for staking.  The primary function of ENG in the Enigma protocol is to pay for computations, very similar to the function of Gas (ether) in the Ethereum network. When an application runs a secret contract on the Enigma network, end users who interact with the application (or the entity that deployed the application) has to spend ENG to get the computation done in a privacy-preserving way. The secondary role of ENG is the Enigma networks utilization of a multi-tiered ENG staking system (secret nodes and eventually consensus nodes). In this system, the more tokens a secret node stakes, the more likely she is to be selected to work on the next secret contract. Nodes acquire and stake ENG to increase their chance of running secret contracts and thus earning a return on their stake.
       </TextContainer>
     </Overview>
